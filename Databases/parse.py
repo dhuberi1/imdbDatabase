9a4be988b8b7ec29099370a0c0bbbd79d181786e
dhuberinbar <huberinbard@gmail.com>
2020-05-06 11:35:13 -0400

    updated parse.py

--- Databases/parse.py
+++ Databases/parse.py
@@ -1,5 +1,7 @@
 import csv
 import sys
+from decimal import Decimal
+import copy
 csv.field_size_limit(sys.maxsize)
 
 titleIDsmall = []
@@ -16,12 +18,13 @@ knownForTitlessmall = [] #this is a list of the unique IDs of movies
 
 # originalTitle = []
 # language = []
-
+ur = []
 titles = []
 uniqID = []
+final = []
+second = []
 
-
-#cleaning title.basics.tsv and turning it into title.basicsCleaned.tsv 
+# #cleaning title.basics.tsv and turning it into title.basicsCleaned.tsv 
 with open("title.basics.tsv", 'r') as fin, open("title.basicsCleaned.tsv", 'w') as fout:
     reader = csv.reader(fin, dialect='excel-tab')
     writer = csv.writer(fout, dialect='excel-tab')
@@ -32,7 +35,7 @@ with open("title.basics.tsv", 'r') as fin, open("title.basicsCleaned.tsv", 'w') 
     	del row[3]
     	writer.writerow(row)
 
-#cleaning title.akas.tsv and turning it into title.akasCleaned.tsv 
+# #cleaning title.akas.tsv and turning it into title.akasCleaned.tsv 
 with open("title.akas.tsv", 'r') as fin, open("title.akasCleaned.tsv", 'w') as fout: 
 	reader = csv.reader(fin, dialect='excel-tab')
 	writer = csv.writer(fout, dialect='excel-tab')
@@ -44,7 +47,7 @@ with open("title.akas.tsv", 'r') as fin, open("title.akasCleaned.tsv", 'w') as f
 		del row[3] #6
 		writer.writerow(row)
 
-#Getting information from title.basicsCleaned 
+# #Getting information from title.basicsCleaned 
 with open("title.basicsCleaned.tsv", 'r') as fin:
 	reader = csv.reader(fin, dialect='excel-tab')
 	count = 0
@@ -58,7 +61,7 @@ with open("title.basicsCleaned.tsv", 'r') as fin:
 		runtimesmall.append(row[3])
 		genressmall.append(row[4])
 
-# Cleaning and creating directors relation
+# # Cleaning and creating directors relation
 with open("title.crew.tsv") as fin, open("dircetors.txt", 'w') as fout: 
 	reader = csv.reader(fin, dialect='excel-tab')
 	writer = csv.writer(fout, dialect='excel-tab')
@@ -72,7 +75,7 @@ with open("title.crew.tsv") as fin, open("dircetors.txt", 'w') as fout:
 		del row[2]
 		writer.writerow(row)
 
-# Cleaning and creating directors-smalls
+# # Cleaning and creating directors-smalls
 with open("title.crew.tsv") as fin, open("dircetors-small.txt", 'w') as fout: 
 	reader = csv.reader(fin, dialect='excel-tab')
 	writer = csv.writer(fout, dialect='excel-tab')
@@ -86,7 +89,54 @@ with open("title.crew.tsv") as fin, open("dircetors-small.txt", 'w') as fout:
 		del row[2]
 		writer.writerow(row)
 
-#Cleaning the file and creating Movies relation 
+# NEW ONE 
+# with open("movies-small.txt", 'r') as fin, open("directors-small.txt", 'w') as fout: 
+# 	reader = csv.reader(fin, dialect='excel-tab')
+# 	writer = csv.writer(fout, dialect='excel-tab')
+# 	count = 0
+# 	length = 0
+# 	for row in reader: 
+# 		if (direcTitlesmall[count] == row[0]):
+# 			# print('hello')
+# 			final.append(direcTitlesmall[count])
+# 			second.append(row[7])
+# 		else: 
+# 			count += 1
+# 		length += 1
+# 	i = 0
+# 	for i in range(len(final)):
+# 		writer.writerow(final[i])
+# 		writer.writerow(second[i])
+# 		i += 1
+
+with open("movies-small.txt", 'r') as fin: 
+	file1 = open("small-direc.txt","a") 
+	reader = csv.reader(fin, dialect='excel-tab')
+	count = 0
+	first = 0
+	li2 = copy.copy(direcTitlesmall)  
+	li2.pop(0)
+	for i in range(30):
+		print(li2[i])
+	next(fin)
+	for row in reader: 
+		if (li2[count] == row[0]):
+			final.append(li2[count])
+			second.append(row[7])
+		else: 
+			count += 1
+			final.append(li2[count])
+			second.append(row[7])
+	i = 0
+
+	for i in range(len(final)):
+		file1.writelines(final[i] + " ")
+		file1.writelines(second[i] + "\n")
+
+		i += 1
+	file1.close
+
+# #Cleaning the file and creating Movies relation 
 with open("title.akasCleaned.tsv", 'r') as fin, open("movies.txt", 'w') as fout: 
 	reader = csv.reader(fin, dialect='excel-tab')
 	writer = csv.writer(fout, dialect='excel-tab')
@@ -111,18 +161,18 @@ with open("title.akasCleaned.tsv", 'r') as fin, open("movies.txt", 'w') as fout:
 			row.append(runtimesmall[index])
 		else: 
 			index += 1
-		print(direc)
+		# print(direc)
 		if (row[0] == direcTitlesmall[direc]):
 			row.append(directorssmall[direc])
 		else: 
 			direc += 1
 		first += 1
 		#just a test
-		if (count == 1000):
-			print(row) 
+		# if (count == 1000):
+		# 	print(row) 
 		writer.writerow(row)
 
-#Creating movies-small
+# #Creating movies-small
 with open("title.akasCleaned.tsv", 'r') as fin, open("movies-small.txt", 'w') as fout: 
 	reader = csv.reader(fin, dialect='excel-tab')
 	writer = csv.writer(fout, dialect='excel-tab')
@@ -152,33 +202,40 @@ with open("title.akasCleaned.tsv", 'r') as fin, open("movies-small.txt", 'w') as
 			row.append(directorssmall[direc])
 		else: 
 			direc += 1
-		#just a test
-		if (count == 10):
-			print('for movies small')
-			print(row) 
-		writer.writerow(row)
+		if (len(row)==8):
+			writer.writerow(row)
 
 # Creating UserRatings relation: 
-with open("title.ratings.tsv", 'r') as fin, open("UserRatings.txt", 'w') as fout: 
-	reader = csv.reader(fin, dialect='excel-tab')
-	writer = csv.writer(fout, dialect='excel-tab')
-	count = 0
-	for row in reader: 
-		count += 1
-		if count > 1100000:
-			break		
-		writer.writerow(row)
+# with open("title.ratings.tsv", 'r') as fin, open("UserRatings.txt", 'w') as fout: 
+# 	reader = csv.reader(fin, dialect='excel-tab')
+# 	writer = csv.writer(fout, dialect='excel-tab')
+# 	count = 0
+# 	for row in reader: 
+# 		count += 1
+# 		if count > 1100000:
+# 			break		
+# 		if count > 1:
+# 			row[1] = Decimal(row[1])
+# 			row[2] = int(row[2])
+# 		writer.writerow(row)
 
 # Creating UserRatings-small relation: 
-with open("title.ratings.tsv", 'r') as fin, open("UserRatings-small.txt", 'w') as fout: 
-	reader = csv.reader(fin, dialect='excel-tab')
-	writer = csv.writer(fout, dialect='excel-tab')
-	count = 0
-	for row in reader: 
-		count += 1
-		if (count > 30):
-			break
-		writer.writerow(row)
+# with open("title.ratings.tsv", 'r') as fin, open("UserRatings-small.txt", 'w') as fout: 
+# 	reader = csv.reader(fin, dialect='excel-tab')
+# 	writer = csv.writer(fout, dialect='excel-tab')
+# 	count = 0
+# 	first = 0
+# 	for row in reader: 
+# 		count += 1
+# 		if (count > 30):
+# 			break
+# 		if (count > 1):
+# 			row[1] = Decimal(row[1])
+# 			row[2] = int(row[2])
+# 			print(type(row[0]))
+# 			print(type(row[1]))
+# 			print(type(row[2]))
+# 		writer.writerow(row)
 
 # Getting Actors and making actors.txt (actors relation): 
 with open("name.basics.tsv", 'r') as fin, open("actors.txt", 'w') as fout: 
@@ -262,36 +319,15 @@ with open("name.basics.tsv", 'r') as fin, open("actors-small.txt", 'w') as fout:
 			del row[1]
 			writer.writerow(row)
 
-# #Cleaning the file and creating Movies relation 
-# with open("title.akas.tsv", 'r') as fin, open("movies.txt", 'w') as fout: 
-# 	reader = csv.reader(fin, dialect='excel-tab')
-# 	writer = csv.writer(fout, dialect='excel-tab')
-# 	index = 0
-# 	direc = 0
-# 	first = 0
-# 	for row in reader: 
-# 		del row[1]
-# 		del row[3]
-# 		del row[3]
-# 		del row[3]
-# 		del row[3]
-# 		if (first == 0):
-# 			row.append('isAdult')
-# 			row.append('startYear')
-# 			row.append('genres')
-# 			row.append('runtime')
-# 			row.append('directors')
-# 		# print(index)
-# 		if (row[0] == titleID[index]):
-# 			row.append(isAdult[index])
-# 			row.append(startYear[index])
-# 			row.append(genres[index])
-# 			row.append(runtime[index])
-# 		else: 
-# 			index += 1
-# 		if (row[0] == direcTitle[direc]):
-# 			row.append(directors[direc])
-# 		else: 
-# 			direc += 1
-# 		# print(row) 
-# 		writer.writerow(row)
+# Removing headers: 
+f = open("actors-small.txt")
+ignore = f.readline()
+open("actors-small.txt", "w+").write(f.read())
+
+f = open("movies-small.txt")
+ignore = f.readline()
+open("movies-small.txt", "w+").write(f.read())
+
+f = open("UserRatings-small.txt")
+ignore = f.readline()
+open("UserRatings-small.txt", "w+").write(f.read())
