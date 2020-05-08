ad24142425209f401c87e193ac3c304181bb50a8
dhuberinbar <huberinbard@gmail.com>
2020-05-08 18:34:14 -0400

    working dbase removed FK

--- Databases/imdb_setup-small.sql
+++ Databases/imdb_setup-small.sql
@@ -1,56 +1,82 @@
 -- Danielle Huber - Inbar Zoe Goldstein
 -- In case we've run this script before, remove old tables before we re-create them
-
+SET FOREIGN_KEY_CHECKS=OFF;
+DROP TABLE IF EXISTS Directors;
 DROP TABLE IF EXISTS Movies;
 DROP TABLE IF EXISTS Actors;
 DROP TABLE IF EXISTS UserReviews;
-DROP TABLE IF EXISTS Directors;
-
+SET FOREIGN_KEY_CHECKS=ON;
 
 CREATE TABLE Directors (
-  titleID VARCHAR(9),
-  directors VARCHAR(30),
-  PRIMARY KEY(titleID)  
+    titleID VARCHAR(9),
+    directors VARCHAR(30),
+    PRIMARY KEY(titleID)  
 );
 
 LOAD DATA LOCAL INFILE 'small-direc.txt' INTO TABLE Directors;
 
 CREATE TABLE Movies (
-  titleID  VARCHAR(9),
-  title    VARCHAR(30),
-  language VARCHAR(2),
-  isAdult  INT,
-  startYear  INT,
-  genres    VARCHAR(30),
-  runtime  INT,
-  directors VARCHAR(30),
-  PRIMARY KEY(titleID, title)
-  FOREIGN KEY(directors) REFERENCES Directors(directors)
+    titleID  VARCHAR(9),
+    title    VARCHAR(30),
+    language VARCHAR(2),
+    isAdult  INT,
+    startYear  INT,
+    genres    VARCHAR(30),
+    runtime  INT,
+    directors VARCHAR(30),
+    PRIMARY KEY(titleID, title)
 );
 
 LOAD DATA LOCAL INFILE 'small-movies.txt' INTO TABLE Movies;
+
+/*
+CREATE TABLE Movies (
+    titleID  VARCHAR(9),
+    title    VARCHAR(30),
+    language VARCHAR(2),
+    isAdult  INT,
+    startYear  INT,
+    genres    VARCHAR(30),
+    runtime  INT,
+    directors VARCHAR(30),
+    PRIMARY KEY(titleID, title),
+    FOREIGN KEY(directors) REFERENCES Directors(directors)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+LOAD DATA LOCAL INFILE 'small-movies.txt' INTO TABLE Movies;
+*/
 --LOAD DATA LOCAL INFILE 'movies-small.txt' INTO TABLE Movies;
 
 CREATE TABLE Actors (
-  name              VARCHAR(30)  NOT NULL,
-  knownForTitles    VARCHAR(100) NOT NULL,
-  age               INT          NOT NULL,
-  PRIMARY KEY(name, knownForTitles)
+    id                VARCHAR(9)    NOT NULL,
+    name              VARCHAR(30)   NOT NULL,
+    knownForTitles    VARCHAR(100)  NOT NULL,
+    age               INT           NOT NULL,
+    PRIMARY KEY(id, name, knownForTitles)
 );
 
 LOAD DATA LOCAL INFILE 'actors-small.txt' INTO TABLE Actors;
 
 CREATE TABLE UserReviews (
-  titleID           VARCHAR(9),
-  averageRating     FLOAT(3),
-  numVotes          INT,
-  PRIMARY KEY(titleID),
-  FOREIGN KEY(titleID) REFERENCES Movies(titleID)
+    titleID          VARCHAR(9),
+    averageRating     FLOAT(3),
+    numVotes          INT,
+    PRIMARY KEY(titleID)
+--    FOREIGN KEY(titleID) REFERENCES Movies(titleID)
 );
 
 LOAD DATA LOCAL INFILE 'UserRatings-small.txt' INTO TABLE UserReviews;
 
 
 -- Example query.
-SELECT *
-FROM Movies;
+/*
+SELECT name
+FROM Actors
+WHERE age > 30;
+*/
+
+SELECT title
+FROM Movies, Directors
+WHERE Movies.directors = Directors.directors;
